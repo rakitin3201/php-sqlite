f5f7f55cd56890ae49f2ce3fa433c397ab7c9bb9
Adama NIADA <a77272199@gmail.com>
2023-03-10 17:23:47 +0000

    hgfd

--- api/index.php
+++ api/index.php
@@ -1,22 +1,10 @@
 <?php
-class MyDB extends SQLite3
-{
-    function __construct()
-    {
-        $this->open('database.sqlite');
-    }
-}
-$db = new MyDB();
+// $db->exec('CREATE TABLE foo (bar STRING)');
+// $db->exec("INSERT INTO foo (bar) VALUES ('This is a test')");
 
-$db->exec('CREATE TABLE foo (bar STRING)');
-$db->exec("INSERT INTO foo (bar) VALUES ('This is a test')");
-
-$result = $db->query('SELECT bar FROM foo');
-var_dump($result->fetchArray());
-$db->close();
-
-
-
+// $result = $db->query('SELECT bar FROM foo');
+// var_dump($result->fetchArray());
+// $db->close();
 
 $db = new SQLite3('/tmp/db.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
