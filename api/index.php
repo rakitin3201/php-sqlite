245d0771cdd2784215164583a5197a85953e8e61
Adama NIADA <a77272199@gmail.com>
2023-03-10 12:54:43 +0000

    Message

--- api/index.php
+++ api/index.php
@@ -8,15 +8,6 @@ $db->query('CREATE TABLE IF NOT EXISTS "visits" (
     "time" DATETIME
 )');
 
-$db->query('CREATE TABLE IF NOT EXISTS "users" (
-    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    "name" VARCHAR,
-    "email" VARCHAR,
-    "password" VARCHAR,
-    "created_at" DATETIME,
-    "updated_at" DATETIME
-)');
-
 $statement = $db->prepare('INSERT INTO "visits" ("url", "time") VALUES (:url, :time)');
 $statement->bindValue(':url', ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? 'https') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 $statement->bindValue(':time', date('Y-m-d H:i:s'));
