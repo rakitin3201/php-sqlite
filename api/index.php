77d47c9dd2083caad63abaeb874d1578657e7ec5
Adama NIADA <a77272199@gmail.com>
2023-03-10 13:07:32 +0000

    Message 1.11

--- api/index.php
+++ api/index.php
@@ -1,4 +1,22 @@
 <?php
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('database.sqlite');
+    }
+}
+$db = new MyDB();
+
+$db->exec('CREATE TABLE foo (bar STRING)');
+$db->exec("INSERT INTO foo (bar) VALUES ('This is a test')");
+
+$result = $db->query('SELECT bar FROM foo');
+var_dump($result->fetchArray());
+$db->close();
+
+
+
 
 $db = new SQLite3('/tmp/db.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
@@ -7,6 +25,7 @@ $db->query('CREATE TABLE IF NOT EXISTS "visits" (
     "url" VARCHAR,
     "time" DATETIME
 )');
+
 
 $statement = $db->prepare('INSERT INTO "visits" ("url", "time") VALUES (:url, :time)');
 $statement->bindValue(':url', ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? 'https') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
